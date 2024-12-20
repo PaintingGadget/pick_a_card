2d568317e0bb9ba88573f0c7c9c33bc74459e7da
PaintingGadget <gdrunkbob@gmail.com>
2024-12-20 16:12:55 +0100

    Adding symbols of card suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 
 values = ['2','3','4','5','6','7','8','9', '10','J','Q','K','A']
-suits = ['Diamonds', 'Clubs', 'Spades', 'Spades']
+suits = ["\u2666", "\u2663", "\u2660", "\u2665"]
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
